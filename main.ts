eaa2d7c745167c769f5436c198b1a93f56d9aa3a
igorbrasileiro <brasileiro456@gmail.com>
2025-04-24 14:01:28 -0300

    add mcp server

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import { Deco as Deco } from "@deco/deco";
+import { mcpServer } from "@deco/mcp";
 import manifest, { Manifest } from "./manifest.gen.ts";
 const deco = await Deco.init<Manifest>({
   manifest,
+  bindings: {
+    useServer: (deco, hono) => {
+      hono.use("/*", mcpServer(deco));
+    },
+  },
 });
 const envPort = Deno.env.get("PORT");
 Deno.serve({ handler: deco.fetch.bind(deco), port: envPort ? +envPort : 8000 });
