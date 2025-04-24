109dbf728363f3c84c206789c6517ef7f680a897
igorbrasileiro <brasileiro456@gmail.com>
2025-04-24 11:46:44 -0300

    minimum setup

--- main.ts
+++ main.ts
@@ -1,13 +1,7 @@
-import "deco/runtime/htmx/FreshHeadCompat.ts";
-import { Layout } from "./_app.tsx";
-import manifest, { Manifest } from "./manifest.gen.ts";
 import { Deco as Deco } from "@deco/deco";
-import { bindings as HTMX } from "@deco/deco/htmx";
+import manifest, { Manifest } from "./manifest.gen.ts";
 const deco = await Deco.init<Manifest>({
   manifest,
-  bindings: HTMX({
-    Layout,
-  }),
 });
 const envPort = Deno.env.get("PORT");
 Deno.serve({ handler: deco.fetch.bind(deco), port: envPort ? +envPort : 8000 });
